79f7d7cb0d7d30301151856191c318b4d33e68a9
CRISTIAN VALDIVIESO VALENZUELA <cjyape@hotmail.com>
2022-02-26 02:28:56 +0100

    Añadiendo el proyecto

---
+++ src/calculadoragui/CalculadoraGUI.java
@@ -0,0 +1,33 @@
+package calculadoragui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+/**
+ * Método principal el cual llama al metodo Interfaz para ejecutar la calculadora
+ * @author Entornos de desarrollo
+ * 
+ */
+public class CalculadoraGUI {
+
+    public static void main(String[] args) {
+     
+         //obtenemos el tipo de interfaz de nuestro Sistema operativo.
+         String actualName = UIManager.getCrossPlatformLookAndFeelClassName();
+         
+      try {
+            UIManager.setLookAndFeel(actualName);
+            Interfaz w = new Interfaz();
+                // TODO code application logic here
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Interfaz.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                Logger.getLogger(Interfaz.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(Interfaz.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedLookAndFeelException ex) {
+                Logger.getLogger(Interfaz.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+}
